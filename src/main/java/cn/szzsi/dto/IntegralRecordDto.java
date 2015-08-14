6355a795ad0a542c34e46e55114128e439ed7854
Yishe <yishe@foxmail.com>
2015-08-14 18:30:43 +0800

    修改

--- src/main/java/cn/szzsi/dto/IntegralRecordDto.java
+++ src/main/java/cn/szzsi/dto/IntegralRecordDto.java
@@ -20,11 +20,30 @@ public class IntegralRecordDto{
             point = 4;
             memo = "首次响应在2分钟之内";
         }else if(deta <= 3*60*1000){
-            point = 4;
-            memo = "首次响应在2分钟之内";
+            point = 3;
+            memo = "首次响应在3分钟之内";
+        }else if(deta <= 4*60*1000){
+            point = 2;
+            memo = "首次响应在4分钟之内";
+        }else if(deta <= 5*60*1000){
+            point = 1;
+            memo = "首次响应在5分钟之内";
+        }else if(deta <= 6*60*1000){
+            point = -2;
+            memo = "首次响应在6分钟之内";
+        }else if(deta <= 7*60*1000){
+            point = -4;
+            memo = "首次响应在7分钟之内";
+        }else if(deta <= 8*60*1000){
+            point = -6;
+            memo = "首次响应在8分钟之内";
+        }else if(deta <= 9*60*1000){
+            point = -8;
+            memo = "首次响应在9分钟之内";
+        }else if(deta <= 10*60*1000){
+            point = -10;
+            memo = "首次响应在10分钟之内";
         }
-
-
     }
 
     public int getPoint(){
